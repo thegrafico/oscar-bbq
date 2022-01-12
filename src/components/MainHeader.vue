cf5285e2d1bf07a55037286138a86949ef4ef3b0
Raul Pichardo <raul022107@gmail.com>
2022-01-11 21:05:40 -0400

    Adding fontawesome

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,85 +1,66 @@
 <template>
   <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-      <!-- <div class="container">
-          <img src="../assets/oscar-bbq-logo.jpeg" alt="oscar-bbq-logo">
-
-          <div class="header-links">
-              <a href="" class="current-link">Inicio</a>
-              <a href="">Asadores</a>
-              <a href="">Galer&iacute;a</a>
-              <a href="">Contacto</a>
-          </div>
-      </div> -->
+    <div class="container">
+      <img src="../assets/oscar-bbq-logo.jpeg" alt="oscar-bbq-logo" />
+      <div class="header-links">
+        <a href="" class="current-link">Inicio</a>
+        <a href="">Asadores</a>
+        <a href="">Galer&iacute;a</a>
+        <a href="">Contacto</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainHeader',
+  name: "MainHeader",
   props: {
-    title: String
-  }
-}
+    title: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.container-fluid{
-    color: white;
-    background-color: #1e1e1e;
-    margin-bottom: 1rem;
-    padding-bottom: .8rem;
+.container-fluid {
+  color: white;
+  background-color: #1e1e1e;
+  margin-bottom: 1rem;
+  padding-bottom: 0.8rem;
 }
 
-.header-links a{
-    text-decoration: none;
-    color: white;
-    margin: 0 1.5rem;
-    font-size: 1.5em;
+.header-links a {
+  text-decoration: none;
+  color: white;
+  margin: 0 1.5rem;
+  font-size: 1.5em;
 }
 
-.header-links a:hover{
-    font-weight: bold;
-    border-bottom: .15rem solid red;
+.header-links a:hover {
+  font-weight: bold;
+  border-bottom: 0.15rem solid red;
 }
 
-.current-link{
-    border-bottom: .15rem solid red;
+.current-link {
+  border-bottom: 0.15rem solid red;
 }
+
+.icon{
+  font-size: 5em;
+}
+
+/* Responsive */
+@media only screen and (max-width: 768px) {
+  .header-links a{
+    background-color: yellow;
+    display: block;
+    visibility: hidden;
+  }
+
+  .header-links a:hover, .current-link{
+      border-bottom: none;
+  }
+}
+
 </style>
